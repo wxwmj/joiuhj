f5db494e9497fd69ef13e1abf66d6be66eacf1b7
wxwmj <71175475+wxwmj@users.noreply.github.com>
2025-05-09 17:47:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -40,8 +40,7 @@ raw_group_links = [
     'https://t.me/oneclickvpnkeys',
     'https://t.me/entryNET',
     'https://t.me/daily_configs',
-    'https://t.me/VPN365R',
-    'https://t.me/ConfigsHUB2',
+     'https://t.me/DailyV2RY',
 ]
 
 # 去重处理，并记录重复项
