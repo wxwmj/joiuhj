aa8a167df0d6ccefcaf9e4ae9d7dac9e4fc95e9f
wxwmj <71175475+wxwmj@users.noreply.github.com>
2025-09-04 13:53:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -33,27 +33,18 @@ logging.basicConfig(
 
 # 群组链接（去重）
 raw_group_links = [
-
+    
     'https://t.me/config_proxy',
+    'https://t.me/Farah_VPN',
+    'https://t.me/Farah_Proxy',
     'https://t.me/free_outline_keys',
-    'https://t.me/freenettir',
-    'https://t.me/v2ray_configs_pool',
-    'https://t.me/VPN365R',
-    'https://t.me/DailyV2RY',
-    'https://t.me/Trick_mobil',
-    'https://t.me/vpnplusee_free',
-    'https://t.me/daily_configs',
-    'https://t.me/oneclickvpnkeys',
-    'https://t.me/V2All',
-    'https://t.me/Outline_FreeKey',
-    'https://t.me/V2ranNG_vpn',
-    'https://t.me/v2rey_grum',
-    'https://t.me/freevpnatm',
+    'https://t.me/sorenab1',
     'https://t.me/VIPV2rayNGNP',
     'https://t.me/Outline_ir',
-    'https://t.me/Farah_VPN',
-    'https://t.me/sorenab1',
-    'https://t.me/wxgmrjdcc',
+    'https://t.me/freevpnatm',
+    'https://t.me/wxgqlfx',
+    'https://t.me/V2All',
+
 ]
 group_links = []
 seen = set()
